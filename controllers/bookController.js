d28c7d980170fc235c62b00246eb4332e120d224
Jeferson Ferreira <fjeferson60@gmail.com>
2022-02-08 22:24:22 -0300

    Adicionando model create book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -82,14 +82,83 @@ exports.book_create_get = ((req, res) => {
   }),
   function (err, results) {
     if(err) { return next(err); }
-    res.render('book_form', { title: 'Create Book', authors: results.authors, genres: results.genres });
+    res.render('book_form', {
+       title: 'Create Book', 
+       authors: results.authors, 
+       genres: results.genres 
+      });
   }
 });
 
 //cria Book em POST
-exports.book_create_post = ((req, res) => {
-  res.send('HELLO with A resource AAAA')
-});
+exports.book_create_post = [
+  (req, res, next) => {
+    if(!(req.body.genre instanceof Array)){
+      if(typeof req.body.genre === 'undefined')
+      req.body.genre = [];
+      else 
+      req.body.gere = new Array(req.body.genre);
+    }
+    next();
+  },
+
+  //validate and sanitize fields
+  body('title', 'Title must not be empty').trim().isLength({ min: 1}).escape(),
+  body('author', 'Author must not be empty.').trim().isLength({ min: 1}).escape(),
+  body('summary', 'Summary must not be empty').trim().isLength({ min: 1}).escape(),
+  body('isbn', 'ISBN must not be empty').trim().isLength({ min: 1}).escape(),
+  body('genre.*').escape(),
+
+  (req, res, next) => {
+
+    const errors = validatorResult(req);
+
+    let book = new Book(
+      { title: req.body.title, 
+        author: req.body.author,
+        summary: req.body.summary,
+        isbn: req.body.isbn,
+        genre: req.body.genre,
+      }
+    );
+
+    if(!errors.isEmpty()) {
+
+      //Get all authors and genres for form 
+      async.parallel({
+        authors: function (callback) {
+          Author.find(callback);
+        },
+        genres: function (callback) {
+          Genre.find(callback);
+        },
+      },
+      function (err, results) {
+        if(err) { return next(err); }
+        //Mark our selected genres as checked
+        for(let i = 0; i < results.genres.length; i++){
+          if(book.genre.indexOf(results.genres[i]._id) > -1){
+            results.genres[i].checked = 'true';
+          }
+        }
+        res.render('book_form', {
+          title: 'Create Book', 
+          authors: results.authors, 
+          genres: results.genres,
+          book: book, 
+        errors: errors.array()
+      });
+    });
+    return;
+    }
+    else {
+      book.save( function(err) {
+        if(err) { return next(err);}
+        res.redirect(book.url);
+      });
+    }
+  }
+];
 
 //exibe exclusão de Book em GET
 exports.book_delete_get = ((req, res) => {
@@ -110,3 +179,6 @@ exports.book_update_get = ((req, res) => {
 exports.book_update_post = ((req, res) => {
   res.send('HELLO')
 });
+
+
+//REVISAR ESTE CODIGO 
